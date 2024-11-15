01591e8b98e7b1436c9e5baa04a960d4d231e768
LeidysMMR <leidysm28@gmail.com>
2024-11-15 13:49:55 +0100

    script

---
+++ Script.js
@@ -0,0 +1,41 @@
+alert("Hola mundo");
+alert(1+9+41);
+alert("1+9+41");
+
+let saludo = "holaaaa";
+
+let despedida;
+despedida = "adios ";
+
+saludo = "hola!";
+
+saludo = despedida;
+
+alert(1+2);//suma
+alert(1-2);//resta
+alert(1/2);//division
+alert(1*2);//multiplicacion
+alert(1%2);//resto de dividir
+
+let nombreusuario = "Leidys";
+
+alert(saludo + nombreusuario);
+
+const SALUDO_USUARIO = "hola de nuevo "nombreusuario; //adios Leidys
+
+
+
+let edad = 20;
+edad = 20;
+edad+1; //puede sumar a la variable porque guarda un valor numerico
+edad++; //suma
+edad--; //resta una
+
+//console () muestra lo que pongamos dentro de los parentesis
+
+console.log(edad);
+
+
+alert(saludo + nombreusuario);
+
+
