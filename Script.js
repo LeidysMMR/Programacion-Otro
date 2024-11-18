eab9720e7629f87e27b5cd055e6dfbdb04f7e896
LeidysMMR <leidysm28@gmail.com>
2024-11-18 14:00:16 +0100

    console script

--- Script.js
+++ Script.js
@@ -1,5 +1,5 @@
 alert("Hola mundo");
-alert(1+9+41);
+alert(1 + 9 + 41);
 alert("1+9+41");
 
 let saludo = "holaaaa";
@@ -11,30 +11,59 @@ saludo = "hola!";
 
 saludo = despedida;
 
-alert(1+2);//suma
-alert(1-2);//resta
-alert(1/2);//division
-alert(1*2);//multiplicacion
-alert(1%2);//resto de dividir
+alert(1 + 2);//suma
+alert(1 - 2);//resta
+alert(1 / 2);//division
+alert(1 * 2);//multiplicacion
+alert(1 % 2);//resto de dividir
 
 let nombreusuario = "Leidys";
 
 alert(saludo + nombreusuario);
 
-const SALUDO_USUARIO = "hola de nuevo "nombreusuario; //adios Leidys
+const SALUDO_USUARIO = "hola de nuevo " + nombreusuario; //adios Leidys
 
 
 
 let edad = 20;
 edad = 20;
-edad+1; //puede sumar a la variable porque guarda un valor numerico
+edad + 1; //puede sumar a la variable porque guarda un valor numerico
 edad++; //suma
 edad--; //resta una
 
 //console () muestra lo que pongamos dentro de los parentesis
 
-console.log(edad);
 
+// operadores logicos
+console.log(edad);
+console.log("hola");
+
+console.log(5 > 4);
+console.log(5 < 8);
+
+console.log(5 >= 6);
+console.log(5 <= 7);
+
+console.log(5 == 5);
+console.log(5 != 7);
+
+console.log("a" > "b");
+
+if (edad >= 18) {
+    alert("eres mayor de edad");
+}
+
+let premium = true;
+
+if (premium) {
+    console.log ("tiene cuenta privada");
+}
+
+//interactuar con html DOM
+document.getElementById("texto").innerHTML = "holaaaaaaaaaaaa "+nombreusuario;
+
+
+document.getElementsByClassName("articulo")
 
 alert(saludo + nombreusuario);
 
